98d6e12b1bfd8cac8651d3c08f5f8054dc84d230
Karl George <kelsyerg@gmail.com>
2017-05-18 20:08:15 +0100

    Working on modal implementation

--- src/js/script.js
+++ src/js/script.js
@@ -10,9 +10,17 @@ closeBtn.forEach(function (close) {
 
 var proj1 = document.getElementById('proj1');
 var proj2 = document.getElementById('proj2');
+var proj3 = document.getElementById('proj3');
+var proj4 = document.getElementById('proj4');
+var proj5 = document.getElementById('proj5');
+var proj6 = document.getElementById('proj6');
 
 var modal1 = document.querySelector('.proj1');
 var modal2 = document.querySelector('.proj2');
+var modal3 = document.querySelector('.proj3');
+var modal4 = document.querySelector('.proj4');
+var modal5 = document.querySelector('.proj5');
+var modal6 = document.querySelector('.proj6');
 
 
 proj1.addEventListener('click', function() {       
@@ -23,4 +31,24 @@ proj1.addEventListener('click', function() {
 proj2.addEventListener('click', function() {    
        modal2.style.display = 'block';
        currentModal = modal2;
+});
+
+proj3.addEventListener('click', function() {    
+       modal3.style.display = 'block';
+       currentModal = modal3;
+});
+
+proj4.addEventListener('click', function() {    
+       modal4.style.display = 'block';
+       currentModal = modal4;
+});
+
+proj5.addEventListener('click', function() {    
+       modal5.style.display = 'block';
+       currentModal = modal5;
+});
+
+proj6.addEventListener('click', function() {    
+       modal6.style.display = 'block';
+       currentModal = modal6;
 });
